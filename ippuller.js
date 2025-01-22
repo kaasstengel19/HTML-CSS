9de678a205d521263009868bfb85bf0727711b34
kaasstengel19 <rickmali@kpnmail.nl>
2025-01-22 08:57:45 +0100

    Create ippuller.js

---
+++ ippuller.js
@@ -0,0 +1,10 @@
+let username = document.getElementById("fname");
+let ww = document.getElementById("lname");
+let Hans = "Hans";
+let Phaedon = "Phaedon";
+function ippuller(){
+    console.log(username.value);
+    if(username.value == Hans && ww.value == Phaedon){
+        document.location.href = "eindopdr.html";
+    }
+}
