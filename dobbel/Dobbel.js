9c4f13ac551ae48c0fe08a9b15f3193c980fc5c9
kaasstengel19 <rickmali@kpnmail.nl>
2025-02-06 13:51:53 +0100

    Update Dobbel.js

--- dobbel/Dobbel.js
+++ dobbel/Dobbel.js
@@ -8,12 +8,21 @@ function Dobbel() {
         'dob6.png'
     ];
 
-    
+    for (let i = 1; i <= 2; i++) {
     const randomIndex = Math.floor(Math.random() * images.length);
 
     
-    const imageElement = document.getElementById('random-image');
+    const imageElement = document.getElementById(`dice${i}`);
 
     
     imageElement.src = images[randomIndex];
+    }
+
+    setTimeout(() => {
+        for (let i = 3; i <= 4; i++) {
+            const randomIndex = Math.floor(Math.random() * images.length);
+            const imageElement = document.getElementById(`dice${i}`);
+            imageElement.src = images[randomIndex];
+        }
+    }, 1000);
 }
